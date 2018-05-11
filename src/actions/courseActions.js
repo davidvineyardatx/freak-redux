4f02af066b42762615ab89bbd27c1d0d5f938f1b
David <david@Administrators-iMac.local>
2018-05-11 10:10:53 -0500

    edited

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -6,11 +6,12 @@ export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses};
 }
 export function createCourseSuccess(course) {
+  console.log("in createCourseSuccess: ");
   return {type: types.CREATE_COURSE_SUCCESS, course};
 }
 
 export function updateCourseSuccess(course) {
-  return {type: types.UPDATE_COURSE_SUCCESS};
+  return {type: types.UPDATE_COURSE_SUCCESS, course};
 }
 
 // Lets thunk
@@ -26,10 +27,12 @@ export function loadCourses() {
 }
 
 export function saveCourse(course) {
+  console.log("in saveCourse Action: ");
   return function (dispatch, getState) {
     dispatch(beginAjaxCall());
     return courseApi.saveCourse(course).then(savedCourse => {
       course.id ? dispatch(updateCourseSuccess(savedCourse)) : // Forking here based on wether a course is passed in
+console.log("SavedCourse: " + savedCourse.id);
         dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
       throw(error);
